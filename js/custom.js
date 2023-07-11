1f6a53aa005317d57bb8a0b9c553b4774df0cb70
liyan-nk <117467536+liyan-nk@users.noreply.github.com>
2023-07-11 18:44:53 +0530

    commits

--- js/custom.js
+++ js/custom.js
@@ -407,3 +407,17 @@ const images = document.querySelectorAll('.container_main img');
          changeImage(1);
       }
    });
+
+
+   // Back to top button
+   $(window).scroll(function () {
+	if ($(this).scrollTop() > 300) {
+		$('.back-to-top').fadeIn('slow');
+	} else {
+		$('.back-to-top').fadeOut('slow');
+	}
+});
+$('.back-to-top').click(function () {
+	$('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+	return false;
+});
