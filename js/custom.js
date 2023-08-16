1b6253d5a01b4932e9f27eb72fa6b1c65911a28c
unknown <lnk.liyannk@gmail.com>
2023-08-16 18:51:17 +0530

    commits

--- js/custom.js
+++ js/custom.js
@@ -444,7 +444,7 @@ function handleDownloadClick1() {
     
 }
 
-function handleDownloadClick2() {
+/* function handleDownloadClick2() {
     const pdfFilePath2 = '../images/offers/DIMOS_NOTICE_ BACK_FINAL.pdf'; // Relative path to the second PDF file
 
 
@@ -462,7 +462,7 @@ function handleDownloadClick2() {
 }
 
 downloadButton1.addEventListener('click', handleDownloadClick1);
-downloadButton2.addEventListener('click', handleDownloadClick2);
+downloadButton2.addEventListener('click', handleDownloadClick2); */
 
 
 
